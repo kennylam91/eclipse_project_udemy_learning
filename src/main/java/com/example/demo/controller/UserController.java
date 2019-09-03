61e9ffdff597d0026e4ba21d3ccbdb3b67b69fe2
kennylam91 <phamvanlam1991@gmail.com>
2019-09-03 16:31:26 +0700

    add getUserByid method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,11 @@ public class UserController {
 	@GetMapping
 	public String getUsers() {
 		return "get users";
+	}
+
+	@GetMapping(path = "/{userId}")
+	public String getUserById(@PathVariable String userId) {
+		return "get user by id = " + userId;
 	}
 
 	@PostMapping
