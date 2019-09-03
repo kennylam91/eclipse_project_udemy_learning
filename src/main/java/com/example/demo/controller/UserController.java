8537501d0327f2f5f27947a79bbccf517ec6f053
kennylam91 <phamvanlam1991@gmail.com>
2019-09-03 16:28:41 +0700

    add updateUser and deleteUser

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,16 @@ public class UserController {
 	@PostMapping
 	public String createUser() {
 		return " create user";
+	}
+
+	@PutMapping
+	public String updateUser() {
+		return " update user";
+	}
+
+	@DeleteMapping
+	public String deleteUser() {
+		return " delete user";
 	}
 
 }
