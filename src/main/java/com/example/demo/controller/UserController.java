316fcf3013d74727f6e196c7085ca1bafa31615f
kennylam91 <phamvanlam1991@gmail.com>
2019-09-03 16:26:56 +0700

    add createUser method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,11 @@ public class UserController {
 	@GetMapping
 	public String getUsers() {
 		return "get users";
+	}
+
+	@PostMapping
+	public String createUser() {
+		return " create user";
 	}
 
 }
